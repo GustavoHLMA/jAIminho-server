0ceff73aa7fcf2d350189f53a584f71e03472e25
Gustavo Henrique <gustavohlma8@gmail.com>
2024-09-06 22:18:37 -0300

    fix(main): buildando pro deploy

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://jaiminho-server.onrender.com',
+    'https://jaiiminho-client.vercel.app/',
+    'https://jaiminho-client.vercel.app/',
+    'https://jaiiminho-client-ly7t0ljzh-gustavohlmas-projects.vercel.app/'
+];
 app.use(express_1.default.json());
 app.use(routes_1.default);
+app.use((0, cors_1.default)({
+    origin: allowedOrigins
+}));
 exports.default = app;
