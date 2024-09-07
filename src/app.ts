7e645c85f045203e2f834eddeefa957e2fff9717
Gustavo Henrique <gustavohlma8@gmail.com>
2024-09-06 22:13:03 -0300

    feat(main): adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://jaiminho-server.onrender.com',
+  'https://jaiiminho-client.vercel.app/',
+  'https://jaiminho-client.vercel.app/',
+  'https://jaiiminho-client-ly7t0ljzh-gustavohlmas-projects.vercel.app/'
+]
+
 app.use(express.json());
 app.use(router);
+app.use(cors({
+  origin: allowedOrigins
+}));
 
 export default app;
